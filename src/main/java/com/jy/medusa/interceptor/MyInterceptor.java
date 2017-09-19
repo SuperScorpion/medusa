d00c6bd7aef74f4fdd4f7a2bace5eec78fbd1411
neo <474538837@qq.com>
2017-09-19 10:21:37 +0800

    重构interceptor core 2.1.4~

--- src/main/java/com/jy/medusa/interceptor/MyInterceptor.java
+++ src/main/java/com/jy/medusa/interceptor/MyInterceptor.java
@@ -17,8 +17,11 @@ import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.defaults.DefaultSqlSession;
 
+import java.lang.reflect.InvocationTargetException;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,113 +40,136 @@ public class MyInterceptor implements Interceptor {
 
         if (invocation.getTarget() instanceof Executor) {
 
-            MappedStatement mt = (MappedStatement) invocation.getArgs()[0];
+            result = processExecutor(invocation);
 
-            String medusaMethodName = MyHelper.getLastWord(mt.getId()).trim();
-
-            if (MyHelper.checkMortalMethds(medusaMethodName)) {//Modify by neo on 2016.10.25
-
-                Map<String, Object> p = new HashMap<>();
-
-                p.put("pobj", invocation.getArgs()[1]);
-
-                p.put("msid", MyHelper.removeLastWord(mt.getId()));
-
-                invocation.getArgs()[1] = p;
-
-                result = invocation.proceed();//mybatis 的后续还有很多处理
-
-                //测试结果由高到低:startWith->indexOf->contains modify by neo on 2016.11.07
-                if (!medusaMethodName.startsWith("select") && !medusaMethodName.startsWith("delete")) {//查询比较多 避免再去判断是不是以下方法中的 提升性能 可以 短路 modify by neo on 2016.11.07
-
-                    if (MyHelper.checkMedusaMethod(medusaMethodName)) {//若是多条件查询 medusas
-
-                        Object[] x = (Object[]) ((DefaultSqlSession.StrictMap) p.get("pobj")).get("array");//modify by neo on 2016.12.23
-
-                        Pager z = null;
-
-                        for (Object m : x) {
-                            if (m instanceof Pager) {
-                                z = (Pager) m;
-                                break;//只要第一个对象 pager
-                            }
-                        }
-
-                        if (z != null && result != null) {//modify by neo on 2016.10.11
-                            z.setList((List) result);//若结果集不为空则 给原有的pager参数注入list属性值
-                            z.setTotalCount(MyHelper.caculatePagerTotalCount(((Executor) invocation.getTarget()).getTransaction().getConnection(), mt, p));/////通过invocation参数获得connection连接 并且通过这个连接查询出totalCount
-                            z.setPageCount(z.getPageCount());
-                        }
-
-                    } else if (MyHelper.checkInsertMethod(medusaMethodName)) {//如果是insert方法相关的则修改传入对象的id 回执其
-
-                        //returns generator id key change return values
-
-                        if (((Map) invocation.getArgs()[1]).get(SystemConfigs.PRIMARY_KEY) != null) {///modify by neo on 2016.10.27 因为有些非自增主键 手动添加的id值 不会返回艾迪
-
-                            Object m = Integer.valueOf((((Map) invocation.getArgs()[1]).get(SystemConfigs.PRIMARY_KEY).toString()));//TODO mybaits long is default id types
-
-                            MyReflectionUtils.invokeSetterMethod(p.get("pobj"), SystemConfigs.PRIMARY_KEY, m);//注入属性id值
-                        }
-
-                    } else if (MyHelper.checkUpdateMethod(medusaMethodName)) {
-
-                        if (result.toString().equals("0"))
-                            throw new MedusaException("Medusa: The update method is there a number of exceptions to zero!(Maybe your incoming primary key is empty please check!)");
-
-                    } else if (MyHelper.checkInsertUUIDMethod(medusaMethodName)) {//modify by neo on 2016.12.17
-
-                        //returns generator id key change return values
-
-                        if (((Map) invocation.getArgs()[1]).get(SystemConfigs.PRIMARY_KEY) != null) {///modify by neo on 2016.10.27 因为有些非自增主键 手动添加的id值 不会返回艾迪
-
-                            Object m = String.valueOf((((Map) invocation.getArgs()[1]).get(SystemConfigs.PRIMARY_KEY).toString()));//TODO mybaits long is default id types
-
-                            MyReflectionUtils.invokeSetterMethod(p.get("pobj"), SystemConfigs.PRIMARY_KEY, m);//注入属性id值
-                        }
-                    } else {
-//                        do nothing
-                    }
-                }
-            } else {///如果为普通方法 自定义
-
-                result = invocation.proceed();
-            }
         } else if(invocation.getTarget() instanceof StatementHandler) {//delete insert update 都会进来此拦截(medusa的或非medusa的)
 
             result = invocation.proceed();//先执行再处理的 不然处理的是上次的
 
-            StatementHandler sh = (StatementHandler) invocation.getTarget();
-
-            if(sh.getBoundSql().getSql().contains("INSERT INTO") && !sh.getBoundSql().getSql().contains("on duplicate key update")) {//过滤掉delete update 批量更新之类的 非insert方法
-
-                Object c = ((Map) sh.getBoundSql().getParameterObject()).containsKey("pobj") ? ((Map) sh.getBoundSql().getParameterObject()).get("pobj") : null;//过滤掉用户自定义的方法
-
-                if (c instanceof Map) {//过滤掉普通插入
-
-                    List<Object> paramList = (List) ((Map) c).get("param1");
-
-                    if (paramList != null && !paramList.isEmpty()) {
-
-                        Statement st = (Statement) invocation.getArgs()[0];
-
-                        ResultSet rs = st.getGeneratedKeys();
-
-                        for (Object ot : paramList) {
-
-                            if (!rs.next()) break;
-
-                            MyReflectionUtils.invokeSetterMethod(ot, SystemConfigs.PRIMARY_KEY, rs.getInt(1));//注入属性id值 rs.getInt(1) 每次执行都取到不同 id 很神奇 (一行多列的原因是吗)
-                        }
-                    }
-                }
-            }
+            processBatchInsertPrimaryKeyWriteBack(invocation);
 
         } else {
             result = invocation.proceed();
         }
 
         return result;
+    }
+
+    private Object processExecutor(Invocation invocation) throws InvocationTargetException, IllegalAccessException, ParseException, SQLException {
+
+        Object result;
+
+        MappedStatement mt = (MappedStatement) invocation.getArgs()[0];
+
+        String medusaMethodName = MyHelper.getLastWord(mt.getId()).trim();
+
+        if (MyHelper.checkMortalMethds(medusaMethodName)) {//Modify by neo on 2016.10.25
+
+            Map<String, Object> p = new HashMap<>();
+
+            p.put("pobj", invocation.getArgs()[1]);
+
+            p.put("msid", MyHelper.removeLastWord(mt.getId()));
+
+            invocation.getArgs()[1] = p;
+
+            result = invocation.proceed();//mybatis 的后续还有很多处理
+
+            processMedusaMethod(medusaMethodName, result, invocation, p, mt);
+
+        } else {///如果为普通方法 自定义
+
+            result = invocation.proceed();
+        }
+
+        return result;
+    }
+
+    private void processMedusaMethod(String medusaMethodName, Object result, Invocation invocation, Map<String, Object> p, MappedStatement mt) throws SQLException, ParseException {
+
+        //测试结果由高到低:startWith->indexOf->contains modify by neo on 2016.11.07
+        if (!medusaMethodName.startsWith("select") && !medusaMethodName.startsWith("delete")) {//查询比较多 避免再去判断是不是以下方法中的 提升性能 可以 短路 modify by neo on 2016.11.07
+
+            if (MyHelper.checkMedusaMethod(medusaMethodName)) {//若是多条件查询 medusas
+
+                Object[] x = (Object[]) ((DefaultSqlSession.StrictMap) p.get("pobj")).get("array");//modify by neo on 2016.12.23
+
+                Pager z = null;
+
+                for (Object m : x) {
+                    if (m instanceof Pager) {
+                        z = (Pager) m;
+                        break;//只要第一个对象 pager
+                    }
+                }
+
+                if (z != null && result != null) {//modify by neo on 2016.10.11
+                    z.setList((List) result);//若结果集不为空则 给原有的pager参数注入list属性值
+                    z.setTotalCount(MyHelper.caculatePagerTotalCount(((Executor) invocation.getTarget()).getTransaction().getConnection(), mt, p));/////通过invocation参数获得connection连接 并且通过这个连接查询出totalCount
+                    z.setPageCount(z.getPageCount());
+                }
+
+            } else if (MyHelper.checkInsertMethod(medusaMethodName)) {//如果是insert方法相关的则修改传入对象的id 回执其
+
+                //returns generator id key change return values
+
+                if (((Map) invocation.getArgs()[1]).get(SystemConfigs.PRIMARY_KEY) != null) {///modify by neo on 2016.10.27 因为有些非自增主键 手动添加的id值 不会返回艾迪
+
+                    Object m = Integer.valueOf((((Map) invocation.getArgs()[1]).get(SystemConfigs.PRIMARY_KEY).toString()));//TODO mybaits long is default id types
+
+                    MyReflectionUtils.invokeSetterMethod(p.get("pobj"), SystemConfigs.PRIMARY_KEY, m);//注入属性id值
+                }
+
+            } else if (MyHelper.checkUpdateMethod(medusaMethodName)) {
+
+                if (result.toString().equals("0"))
+                    throw new MedusaException("Medusa: The update method is there a number of exceptions to zero!(Maybe your incoming primary key is empty please check!)");
+
+            } else if (MyHelper.checkInsertUUIDMethod(medusaMethodName)) {//modify by neo on 2016.12.17
+
+                //returns generator id key change return values
+
+                if (((Map) invocation.getArgs()[1]).get(SystemConfigs.PRIMARY_KEY) != null) {///modify by neo on 2016.10.27 因为有些非自增主键 手动添加的id值 不会返回艾迪
+
+                    Object m = String.valueOf((((Map) invocation.getArgs()[1]).get(SystemConfigs.PRIMARY_KEY).toString()));//TODO mybaits long is default id types
+
+                    MyReflectionUtils.invokeSetterMethod(p.get("pobj"), SystemConfigs.PRIMARY_KEY, m);//注入属性id值
+                }
+            } else {
+//                        do nothing
+            }
+        } else {
+//                        do nothing
+        }
+    }
+
+    private void processBatchInsertPrimaryKeyWriteBack(Invocation invocation) throws SQLException, ParseException {
+
+        StatementHandler sh = (StatementHandler) invocation.getTarget();
+
+        if(sh.getBoundSql().getSql().contains("INSERT INTO") && !sh.getBoundSql().getSql().contains("on duplicate key update")) {//过滤掉delete update 批量更新之类的 非insert方法
+
+            Object c = ((Map) sh.getBoundSql().getParameterObject()).containsKey("pobj") ? ((Map) sh.getBoundSql().getParameterObject()).get("pobj") : null;//过滤掉用户自定义的方法
+
+            if (c instanceof Map) {//过滤掉普通插入
+
+                List<Object> paramList = (List) ((Map) c).get("param1");
+
+                if (paramList != null && !paramList.isEmpty()) {
+
+                    Statement st = (Statement) invocation.getArgs()[0];
+
+                    ResultSet rs = st.getGeneratedKeys();
+
+                    for (Object ot : paramList) {
+
+                        if (!rs.next()) break;
+
+                        MyReflectionUtils.invokeSetterMethod(ot, SystemConfigs.PRIMARY_KEY, rs.getInt(1));//注入属性id值 rs.getInt(1) 每次执行都取到不同 id 很神奇 (一行多列的原因是吗)
+                    }
+                }
+            }
+        }
     }
 
     public Object plugin(Object target) {
