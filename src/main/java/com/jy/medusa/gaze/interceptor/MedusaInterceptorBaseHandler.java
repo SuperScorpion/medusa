52fba9968f9aa5b38b6e31d3679f7d26928a79dd
lijun <474538837@qq.com>
2022-09-01 16:52:23 +0800

    重构拦截器模块 3.1.8~

--- src/main/java/com/jy/medusa/gaze/interceptor/MedusaInterceptorBaseHandler.java
+++ src/main/java/com/jy/medusa/gaze/interceptor/MedusaInterceptorBaseHandler.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
 
-public abstract class MedusaInterceptorBaseHandler {
+abstract class MedusaInterceptorBaseHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(MedusaInterceptorBaseHandler.class);
 
--- src/main/java/com/jy/medusa/gaze/interceptor/MedusaInterceptorExecutorHandler.java
+++ src/main/java/com/jy/medusa/gaze/interceptor/MedusaInterceptorExecutorHandler.java
@@ -24,7 +24,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public abstract class MedusaInterceptorExecutorHandler extends MedusaInterceptorStatementHandler {
+abstract class MedusaInterceptorExecutorHandler extends MedusaInterceptorStatementHandler {
 
     /**
      * 处理拦截器 Executor 的逻辑
