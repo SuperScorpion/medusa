feb090d69bdd3b3cd3bb979eff89385ca75523d5
lijun <474538837@qq.com>
2025-09-07 11:51:18 +0800

    3.3.0版本更新～

--- src/main/java/com/jy/medusa/gaze/interceptor/MedusaInterceptorBaseHandler.java
+++ src/main/java/com/jy/medusa/gaze/interceptor/MedusaInterceptorBaseHandler.java
@@ -4,6 +4,7 @@ package com.jy.medusa.gaze.interceptor;
  * Created by SuperScorpion on 16/9/15.
  */
 
+import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.Invocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,6 +28,9 @@ abstract class MedusaInterceptorBaseHandler {
             result = invocation.proceed();
             long endTime = System.nanoTime();
             logger.debug("Medusa: SQL运行时间 - " + (endTime - startTime) + "ns" + " - " + (endTime - startTime)/1000000 + "ms");
+
+            //大于10秒则记录为慢查询
+            if((endTime - startTime)/1000000000 > 10) logger.warn("Medusa: 有慢查询出现 - sql - " + ((MappedStatement) invocation.getArgs()[0]).getSqlSource().getBoundSql(invocation.getArgs()[1]).getSql());
         } else {
             result = invocation.proceed();
         }
